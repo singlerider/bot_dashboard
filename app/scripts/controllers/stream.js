50d54e63a46100d9400b97314f772e712348a1fb
Jeremy Stover (Kadajett) <jeremy.ryan.stover@gmail.com>
2015-12-05 00:05:08 -0800

    added chat into the channel stream page

--- app/scripts/controllers/stream.js
+++ app/scripts/controllers/stream.js
@@ -25,8 +25,12 @@ angular.module('App')
       return 'http://player.twitch.tv/?channel="' + $scope.channelName + '"';
     };
     $scope.embed = function(){
+      var chat = $('<iframe src="http://www.twitch.tv/lifewithlaughs/chat?popout=" frameborder="0" scrolling="no" height="500" width="350"></iframe>');
       var stream = $('<iframe src="http://player.twitch.tv/?channel=' + $scope.channelName + '" frameborder="0" scrolling="no" height="378" width="620"></iframe><a href="http://www.twitch.tv/' + $scope.channelName + '?tt_medium=live_embed&tt_content=text_link" style="padding:2px 0px 4px; display:block; width:345px; font-weight:normal; font-size:10px;text-decoration:underline;">Watch live video from ' + $scope.channelName + ' on www.twitch.tv</a>');
       console.log('stream: ', stream);
+
+      $('#chatContainer').empty();
+      $('#chatContainer').append(chat);
       $('#streamContainer').empty();
       $('#streamContainer').append(stream);
 
