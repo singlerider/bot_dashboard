03871ef22f42664e2bad2b9c3691061874eee003
Shane <haroboy876@gmail.com>
2015-12-21 20:35:30 -0800

    remove old files

--- client/app/modules/stream/config/stream.config.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  'use strict';
-  angular
-    .module('com.module.stream')
-    .run(function ($rootScope, Event, gettextCatalog) {
-      $rootScope.addMenu(gettextCatalog.getString('Stream'), 'app.stream.models', 'fa-globe');
-    });
-
-})();
